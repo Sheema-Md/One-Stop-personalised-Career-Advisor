cfeafcd933b49a1a2e1dbb52b24aaad313556882
Sheema-Md <sheemasadiya688@gmail.com>
2025-09-28 22:21:22 +0530

    github pages linked

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base :'/One-Stop-personalised-Career-Advisor/',
   server: {
     host: "::",
     port: 8080,
